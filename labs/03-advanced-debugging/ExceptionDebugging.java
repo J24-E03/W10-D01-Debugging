b35b60034d10d9e7d6e1329cb388f0c4487dbac6
KaiWelu <kaiweluda@gmail.com>
2024-12-02 12:02:06 +0100

    adds tasks

---
+++ labs/03-advanced-debugging/ExceptionDebugging.java
@@ -0,0 +1,7 @@
+public class ExceptionDebugging {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 2;
+        int result = num1 / num2;  // This will cause an ArithmeticException
+    }
+}
