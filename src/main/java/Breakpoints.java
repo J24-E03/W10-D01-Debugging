87b6ba2c645d07289cd68d7614d2afc26fdf5655
sumayya <sumayyasafoora0@gmail.com>
2024-12-02 14:07:43 +0100

    W10-D01-Debugging Done

---
+++ src/main/java/Breakpoints.java
@@ -0,0 +1,13 @@
+public class Breakpoints {
+    public static void main(String[] args) {
+        int num1 = 2;
+        int num2 = 3;
+//program with a logic error.
+        //int sum = num1 * num2; the output was 6
+
+      //program with a Corrected logic
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is:  " + sum);
+
+    }
+}
